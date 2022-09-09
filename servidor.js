7eb1ef66dffcc11568c26176470f5457c6e0154a
Patriciodiazp <patriciodiapz@gmail.com>
2022-09-08 22:36:23 -0300

    segundo

--- servidor.js
+++ servidor.js
@@ -1,30 +1,42 @@
 const express = require('express')
 const app = express()
-
-app.use(express.urlencoded());
+const productos = []
+app.use(express.urlencoded( {extended: true}));
 app.use(express.json());
-
-app.get('/', function (req, res) {
-    res.send('Hola GeoAPI!');
-  });
+const DB = new db("data");
   
-app.get('/productos', (req, res) => res.send('productos'));
-
-app.get('/productos/:id', (req, res) =>{
-const { id } = req.query;
-res.send(id) 
+app.get("/productos", async (req, res) => {
+  const data = await DB.getAllProduct();
+  return res.send(data);
 });
 
-app.post('/api/productos', (req, res) =>{
+app.get("/api/productos/:id", async (req, res) => {
+  const { id } = req.query;
+  try {
+    const data = await DB.getProductById(id);
 
-})
+    return res.send(data);
+  } catch (e) {
+    return res.status(404).send({ error: true, msg: 'producto no encontrado' });
+  }
+});
+
+app.post("/api/productos", async (req, res) => {
+  const { nombre, precio } = req.body;
+
+  const data = await DB.agregarProducto({ nombre, precio });
+  return res.send({ error: false, msg: "Producto agregado" });
+});
+
 
 app.put('/api/productos/:id',(req, res) =>{
+  const { id } = req.body
+  res.send(data)
 
 })
 
 app.delete('/api/productos/:id', (req, res) =>{
-
+id == req.params.id
 })
 
 
